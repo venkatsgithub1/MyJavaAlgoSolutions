94f28722579be3525a568a829196d230c6206a2f
venkatsgithub1 <venkatsgithub@gmail.com>
2017-01-13 19:01:36 +0530

    Create Anagrams.java

---
+++ hr/Anagrams.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Anagrams {
+    public static int numberNeeded(String first, String second) {
+      
+    }
+  
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        String b = in.next();
+        System.out.println(numberNeeded(a, b));
+    }
+}
